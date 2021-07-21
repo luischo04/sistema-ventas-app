94d7eda628a61bf9b7a5f9e78c2bf06ce6ec12b6
luischo04 <buenrostroluisfernando04@gmail.com>
2021-07-21 12:42:57 -0500

    validacionesLogin
    Se ha añadido:
    * Validaciones de campos login.
    * Header interactivo de acuerdo a si esta logueado o no.

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,33 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
+  private subscriptions: Subscription = new Subscription();
   loginForm = this.fb.group({
-    usuario: [''],
-    password: ['']
+    usuario: ['', [
+      Validators.email,
+      Validators.required,
+    ]],
+    password: ['', [
+      Validators.required,
+      Validators.minLength(4)
+    ]]
   });
 
   constructor(private authSvc: AuthService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
-    
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   onLogin(): void {
     const formValue = this.loginForm.value;
-    this.authSvc.logIn(formValue).subscribe((res) => {
-      if(res) {
-        this.router.navigate(['']);
+    
+    this.subscriptions.add(
+      this.authSvc.logIn(formValue).subscribe((res) => {
+        if (res) {
+          this.router.navigate(['']);
+        }
+      })
+    );
+  }
+
+  getErrorMessage(field: string): string {
+    let message = "";
+    const campo = this.loginForm?.get(field);
+
+    if(campo != null){
+      if(campo.errors?.required){
+        message = "Este campo es requerido";
+      } else if(campo.errors?.email){
+        message = "El formato no es correcto";
+      } else if(campo.errors?.minlength){
+        message = "Los caracteres minimos son 4";
       }
-    });
+    }
+    return message;
   }
 
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,22 +1,30 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { AuthService } from '@app/pages/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  isAdmin = true;
+  private subscriptions: Subscription = new Subscription();
+  isAdmin = false;
   isLogged = false;
   @Output() toggleSidenav = new EventEmitter<void>();
 
-  constructor(private authSvc: AuthService) { 
+  constructor(private authSvc: AuthService) {
   }
 
   ngOnInit(): void {
-    this.authSvc.isLogged.subscribe( res => this.isLogged = res);
+    this.subscriptions.add(
+      this.authSvc.isLogged.subscribe(res => this.isLogged = res)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   onToggleSidenav(): void {
